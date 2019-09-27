3155fb585051d5429f6c6d51f6598c2e864a7946
Noé Falzon <noe@yousician.com>
2019-09-27 14:05:18 +0300

    Added lua server

---
+++ onitama/judge.lua
@@ -0,0 +1,228 @@
+local socket = require "socket"
+local onitama = require "onitama"
+
+local function playerName(player)
+	return player == onitama.Top and "Top" or "Bottom"
+end
+
+local columnNames =
+{
+	a = 1,
+	b = 2,
+	c = 3,
+	d = 4,
+	e = 5,
+	[1] = "a",
+	[2] = "b",
+	[3] = "c",
+	[4] = "d",
+	[5] = "e"
+}
+
+local function moveToString(move)
+	return move.card .. " " ..
+		columnNames[move.from[2]] .. move.from[1] ..
+		" " ..
+		columnNames[move.to[2]] .. move.to[1]
+end
+
+local function startServer(PORT)
+
+	local PORT = PORT or 8000
+	local server = socket.bind("*", PORT)
+
+	if not server then
+		error("Could not start server on port " .. PORT)
+	end
+
+	print("Server running on port " .. PORT)
+
+	local clients = {}
+
+	while #clients < 2 do
+
+		local client = server:accept()
+		local ip,port = client:getpeername()
+		client:settimeout(0.1)
+
+		table.insert(clients, client)
+
+		local player = #clients == 1 and onitama.Top or onitama.Bottom
+		clients[client] = player
+
+		print(playerName(player) .. " player connected: " .. ip)
+		client:send("You are " .. playerName(player) .. "\n")
+		
+	end
+
+	return clients
+end
+
+local function printState(game)
+
+	print "======\n"
+	print(onitama.stateToString(game))
+	print ""
+
+	io.write("Valid moves: ")
+
+	local moves = onitama.validMoves(game)
+	for i,move in ipairs(moves) do
+		io.write(moveToString(move), i == #moves and "\n" or " / ")
+	end
+
+end
+
+local function startGame(clients)
+
+	math.randomseed(os.time())
+
+	local cardNames = {}
+	for k,v in pairs(onitama.Cards) do
+		table.insert(cardNames, k)
+	end
+
+	local selected = {}
+	repeat 
+		local i = math.random(#cardNames)
+		table.insert(selected, cardNames[i])
+		table.remove(cardNames, i)
+	until #selected == 5
+
+	local startPlayer = math.random(2)
+
+	local game = onitama.StartState
+
+	game.topCards[1] = selected[1]
+	game.topCards[2] = selected[2]
+	game.bottomCards[1] = selected[3]
+	game.bottomCards[2] = selected[4]
+	game.nextCard = selected[5]
+	
+	game.currentPlayer = startPlayer == 1 and onitama.Top or onitama.Bottom
+	
+	local msg = "Cards: " .. table.concat(selected, ",") .. "\n" ..
+		playerName(game.currentPlayer) .. " starts\n"
+
+	for i,client in ipairs(clients) do
+		client:send(msg)
+	end
+	
+	print(playerName(game.currentPlayer) .. " starts")
+	printState(game)
+	
+	return game
+end
+
+local function handleInput(game, from, msg, clients)
+	
+	-- Check current player
+	
+	if game.currentPlayer ~= from then
+		print(playerName(from) .. " player (not their turn) says: " .. msg)
+		return
+	end
+	
+	-- Parse input
+	
+	local card, ocol, orow, dcol, drow = msg:match("^(%w+) (%a)(%d) (%a)(%d)$")
+	
+	if not (card and ocol and orow and dcol and drow) then
+		print(playerName(from) .. " player says: " .. msg)	
+		return
+	end
+	
+	-- To numbers
+	orow = tonumber(orow)
+	drow = tonumber(drow)
+	ocol = columnNames[ocol]
+	dcol = columnNames[dcol]
+	
+	-- Move validity
+	
+	for _,move in ipairs(onitama.validMoves(game)) do
+		
+		if move.card == card and
+			move.from[1] == orow and
+			move.from[2] == ocol and
+			move.to[1] == drow and
+			move.to[2] == dcol then
+			
+			print(playerName(from) .. " player plays ", msg)
+			
+			onitama.applyMove(game, move)
+			printState(game)
+			
+			for _,client in ipairs(clients) do
+				client:send(msg .. "\n")
+			end
+			
+			if onitama.getWinner(game) then
+				return "abort"
+			end
+			
+			return "played"
+		end
+	end
+	
+	print(playerName(from) .. " player submitted an invalid move: ", msg)
+	return "abort"
+end
+
+local function run(game, clients, timeout)
+
+	local turnStart = os.time()
+
+	while true do
+		
+		if os.time() - turnStart > timeout then
+			print(playerName(game.currentPlayer) .. " took too long to play")
+			return
+		end
+
+		for i,client in ipairs(clients) do
+
+			local msg, err = client:receive("*l")
+
+			if err == "closed" then
+
+				print(playerName(clients[client]) .. " player dropped")
+				return
+
+			elseif err == "timeout" then
+
+			else
+				local status = handleInput(game, clients[client], msg, clients)
+				
+				if status == "abort" then
+					return
+				end
+				
+				if status == "played" then
+					turnStart = os.time()
+				end
+			end
+
+		end
+
+	end
+
+end
+
+function main(args)
+
+	if not args[1] or not args[2] then
+		print("Usage: lua judge.lua <port> <timeout>")
+		return
+	end
+	
+	local timeout = tonumber(args[2])
+	print("Timeout set to " .. timeout .. " seconds")
+	
+	local clients = startServer(args[1])
+	local game = startGame(clients)
+	run(game, clients, timeout)
+
+end
+
+main({...})
---
+++ onitama/onitama.lua
@@ -0,0 +1,279 @@
+-- Utils
+
+local Top = 1
+local Bottom = -1
+
+local Student = 1
+local Master = 2
+
+local Empty = 0
+
+local samePlayer = function(a,b)
+	return a * b > 0
+end
+
+local function tableCopy(t)
+
+	if type(t) ~= "table" then
+		return t
+	end
+
+	local res = {}
+	for k,v in pairs(t) do
+		res[k] = tableCopy(v)
+	end
+
+	return res
+end
+
+-- Cards
+
+local Cards =
+{
+	Tiger = {{-2,0},{1,0}},
+	Crab = {{-1,0},{0,-2},{0,2}},
+	Monkey = {{-1,-1},{-1,1},{1,-1},{1,1}},
+	Crane = {{-1,0},{1,-1},{1,1}},
+	Dragon = {{-1,-2},{-1,2},{1,-1},{1,1}},
+	Elephant = {{-1,-1},{-1,1},{0,-1},{0,1}},
+	Mantis = {{-1,-1},{-1,1},{0,1}},
+	Boar = {{-1,0},{0,-1},{0,1}},
+	Frog = {{-1,-1},{0,-2},{1,1}},
+	Goose = {{-1,-1},{0,-1},{0,1},{1,1}},
+	Horse = {{-1,0},{0,-1},{1,0}},
+	Eel = {{-1,-1},{0,1},{1,-1}},
+	Rabbit = {{-1,1},{0,2},{1,-1}},
+	Rooster = {{-1,1},{0,-1},{0,1},{1,-1}},
+	Ox = {{-1,0},{0,1},{1,0}},
+	Cobra = {{-1,1},{0,-1},{1,1}}
+}
+
+-- State definition
+
+local StartState =
+{
+	topCards = {"Tiger", "Crab"},
+	bottomCards = {"Dragon", "Crane"},
+	nextCard = "Monkey",
+	currentPlayer = Bottom,
+	grid =
+	{
+		{1,1,2,1,1},
+		{0,0,0,0,0},
+		{0,0,0,0,0},
+		{0,0,0,0,0},
+		{-1,-1,-2,-1,-1}
+	}
+}
+
+-- Properties
+
+local hasMaster = function(state,player)
+	
+	for _,line in ipairs(state.grid) do
+		for _,cell in ipairs(line) do
+			if cell == Master * player then
+				return true
+			end
+		end
+	end
+	return false
+end
+
+local getWinner = function(state)
+	
+	if state.grid[1][3] == Bottom * Master then
+		return Bottom
+		
+	elseif state.grid[5][3] == Top * Master then
+		return Top
+		
+	elseif not hasMaster(state,Top) then
+		return Bottom
+	
+	elseif not hasMaster(state,Bottom) then
+		return Top
+		
+	else
+		return nil
+	end
+end
+
+local getPawns = function(state,player) 
+	
+	local res = {}
+	
+	for row,line in ipairs(state.grid) do
+		for col,cell in ipairs(line) do
+			if samePlayer(cell,player) then
+				res[#res+1] = {row,col}
+			end
+		end
+	end
+	return res
+end
+
+local validPosition = function(row,col)
+	return row >= 1 and row <= 5 and col >= 1 and col <= 5
+end
+
+-- Moves
+
+local validMoves = function(state)
+	
+	local res = {}
+	
+	if getWinner(state) then
+		return res
+	end
+	
+	local player = state.currentPlayer
+	local cards = player == Top and state.topCards or state.bottomCards
+	
+	for _,card in ipairs(cards) do
+		for _,pawn in ipairs(getPawns(state,player)) do
+			for _,offset in ipairs(Cards[card]) do
+				
+				local orow,ocol = offset[1],offset[2]
+				if player == Top then
+					orow,ocol = -orow,-ocol
+				end
+				
+				local drow,dcol = pawn[1] + orow, pawn[2] + ocol
+				if validPosition(drow,dcol) then
+					
+					local dcell = state.grid[drow][dcol]
+					if not samePlayer(player,dcell) then
+						res[#res + 1] =
+						{
+							card = card,
+							from = pawn,
+							to = {drow,dcol}
+						}
+					end
+				end
+			end
+		end		
+	end
+	
+	return res
+end
+
+local applyMove = function(state, move)
+	
+	local destPawn = state.grid[move.to[1]][move.to[2]]
+	local origPawn = state.grid[move.from[1]][move.from[2]]
+	
+	-- Move
+	
+	state.grid[move.to[1]][move.to[2]] = origPawn
+	state.grid[move.from[1]][move.from[2]] = Empty
+	
+	-- Cards
+
+	local cards = state.currentPlayer == Top and state.topCards or state.bottomCards
+	if cards[1] == move.card then
+		cards[1] = state.nextCard
+	else
+		cards[2] = state.nextCard
+	end
+	
+	local cardReceived = state.nextCard
+	state.nextCard = move.card
+	
+	-- Player
+	
+	state.currentPlayer = -state.currentPlayer
+	
+	-- Return info for reversibility
+	
+	return destPawn, cardReceived
+end
+
+local undoMove = function(state, move, capture, received)
+	
+	local pawn = state.grid[move.to[1]][move.to[2]]
+	
+	-- Unmove
+	
+	state.grid[move.from[1]][move.from[2]] = pawn
+	state.grid[move.to[1]][move.to[2]] = capture
+
+	-- Unplayer
+	
+	state.currentPlayer = -state.currentPlayer
+	
+	-- Uncard
+	
+	local cards = state.currentPlayer == Top and state.topCards or state.bottomCards
+	
+	if cards[1] == received then
+		cards[1] = move.card
+	else
+		cards[2] = move.card
+	end
+	
+	state.nextCard = received
+end
+
+-- Debug drawing
+
+local pawnToString =
+{
+	[1] = "t",
+	[2] = "T",
+	[-1] = "b",
+	[-2] = "B"
+}
+
+local gridToString = function(grid)
+	local res = ""
+	for _,line in ipairs(grid) do
+		for _,cell in ipairs(line) do
+			res = res .. (pawnToString[cell] or ".")
+		end
+		res = res .. "\n"
+	end
+	return res
+end
+
+local stateToString = function(state)
+	local w = getWinner(state)
+	
+	return
+		table.concat(state.topCards, ",") .. 
+		(state.currentPlayer == Top and (",[" .. state.nextCard .. "]") or "") ..
+		(w == Top and " WIN" or "") ..
+		"\n" ..
+		gridToString(state.grid) ..
+		table.concat(state.bottomCards, ",") ..
+		(state.currentPlayer == Bottom and (",[" .. state.nextCard .. "]") or "") ..
+		(w == Bottom and " WIN" or "")
+		
+end
+
+local moveToString = function(move)
+	return move.card .. " " ..
+		"(" .. move.from[1] .. "," .. move.from[2] .. ") -> " ..
+		"(" .. move.to[1] .. "," .. move.to[2] .. ")"
+end
+
+
+return
+{
+	Cards = Cards,
+	StartState = StartState,
+	samePlayer = samePlayer,
+	Top = Top,
+	Bottom = Bottom,
+	Student = Student,
+	Master = Master,
+	Empty = Empty,
+	getWinner = getWinner,
+	validMoves = validMoves,
+	applyMove = applyMove,
+	undoMove = undoMove,
+	copyState = tableCopy,
+	stateToString = stateToString,
+	moveToString = moveToString
+}
