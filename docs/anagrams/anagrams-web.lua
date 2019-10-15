147d23756de4e5d71e040bcad0716f655449d84b
Noé Falzon <noe@yousician.com>
2019-10-15 11:36:46 +0300

    Published

--- docs/anagrams/anagrams-web.lua
+++ docs/anagrams/anagrams-web.lua
@@ -26,7 +26,7 @@ local function on_dict_loaded(str)
 	end
 
 	dict = anagrams.load_dict(words)
-	
+
 	loading_div.style.display = "none"
 	ui_div.style.display = "block"
 end
@@ -46,197 +46,197 @@ local function load_dict()
 end
 
 local function clearResults()
-	
+
 	while results_div.firstChild ~= js.null do
 		results_div:removeChild(results_div.firstChild)
 	end
-	
+
 end
 
 local add_include, add_exclude
 
 local function create_result_word(str)
-	
+
 	local elem = resultbox_model:cloneNode(true)
-	
+
 	elem.children[0].innerHTML = str
-	
+
 	elem.children[1].onclick = function()
 		add_include(str)
 	end
-	
+
 	elem.children[2].onclick = function()
 		add_exclude(str)
 	end
-	
+
 	return elem
 end
 
 local function add_result(t)
-	
+
 	local div = js.global.document:createElement "div"
 	div.classList:add "resultline"
-	
+
 	for i,v in ipairs(t) do
 		local elem = create_result_word(v)
 		div:appendChild(elem)
 	end
 
 	results_div:appendChild(div)
-	
+
 end
 
 local progress_search
 
 progress_search = function()
-	
+
 	if not current_search then
 		return
 	end
-	
+
 	local result = current_search()
 	if not result then
 		current_search = nil
 		add_result {"(done in " .. (os.time() - start_time) .. " sec.)"}
-		
+
 		body.classList:remove "loading"
-		
+
 		return
 	end
-	
+
 	if result ~= "pause" then
-		
+
 		local t = {}
-		
+
 		for i = 0, includes_div.children.length - 1 do
 			table.insert(t, includes_div.children[i].children[0].innerHTML)
 		end
-		
+
 		for _,word in ipairs(result) do
 			table.insert(t, word.string)
 		end
-		
+
 		add_result(t)
 	end
-	
+
 	js.global:setTimeout(progress_search, 1)
 end
 
 local function restart_search()
-	
+
 	clearResults()
 	current_search = nil
 	body.classList:remove "loading"
-	
+
 	local phrase = phrase_input.value
-	
+
 	local includes = {}
-	
+
 	for i = 0, includes_div.children.length - 1 do
 		table.insert(includes, includes_div.children[i].children[0].innerHTML)
 	end
-	
+
 	local excludes = {}
-	
+
 	for i = 0, excludes_div.children.length - 1 do
 		table.insert(excludes, excludes_div.children[i].children[0].innerHTML)
 	end
-	
+
 	local min_len = tonumber(length_input.value)
-	
+
 	current_search = anagrams.find(dict, phrase, includes, excludes, min_len or 1, "yield_often")
-	
+
 	if not current_search then
 		add_result {"(invalid includes)"}
 		return
 	end
-	
+
 	body.classList:add "loading"
 	start_time = os.time()
-	
+
 	js.global:setTimeout(progress_search, 1)
 end
 
 add_include = function(str)
-	
+
 	local elem = wordbox_model:cloneNode(true)
-	
+
 	-- text
 	elem.children[0].innerHTML = str
-	
+
 	-- hide dot
 	elem.children[1].onclick = function()
 		includes_div:removeChild(elem)
 		restart_search()
 	end
-	
+
 	-- exclude dot
 	elem.children[3].onclick = function()
 		includes_div:removeChild(elem)
 		add_exclude(str)
 	end
-	
+
 	-- include dot
 	elem:removeChild(elem.children[2])
-	
+
 	elem.classList:add "include"
-	
+
 	includes_div:appendChild(elem)
-	
+
 	restart_search()
 end
 
 add_exclude = function(str)
 
 	local elem = wordbox_model:cloneNode(true)
-	
+
 	-- text
 	elem.children[0].innerHTML = str
-	
+
 	-- hide dot
 	elem.children[1].onclick = function()
 		excludes_div:removeChild(elem)
 		restart_search()
 	end
-	
+
 	-- include dot
 	elem.children[2].onclick = function()
 		excludes_div:removeChild(elem)
 		add_include(str)
 	end
-	
+
 	-- exclude dot
 	elem:removeChild(elem.children[3])
-	
+
 	elem.classList:add "exclude"
-	
+
 	excludes_div:appendChild(elem)
-	
+
 	restart_search()
 end
 
 local function on_includes_input()
-	
-	local word = includes_input.value	
+
+	local word = includes_input.value
 	includes_input.value = ""
-	
+
 	if word ~= "" then
 		add_include(word)
 	end
 end
 
 local function on_excludes_input()
-	
-	local word = excludes_input.value	
+
+	local word = excludes_input.value
 	excludes_input.value = ""
-	
+
 	if word ~= "" then
 		add_exclude(word)
 	end
 end
 
 local function setup()
-	
+
 	loading_div = js.global.document:getElementById "loading"
 	ui_div = js.global.document:getElementById "ui"
 	phrase_input = js.global.document:getElementById "phrase_input"
@@ -250,10 +250,10 @@ local function setup()
 
 	wordbox_model = includes_div.firstElementChild
 	includes_div:removeChild(wordbox_model)
-	
+
 	resultbox_model = results_div.firstElementChild
 	results_div:removeChild(resultbox_model)
-	
+
 	phrase_input.onchange = restart_search
 	includes_input.onchange = on_includes_input
 	excludes_input.onchange = on_excludes_input
--- docs/anagrams/anagrams.lua
+++ docs/anagrams/anagrams.lua
@@ -12,12 +12,11 @@ end
 local function load_word(w)
 
 	local res = {}
-	local space = utf8.codepoint " "
 
-	for pos,code in utf8.codes(w) do
-		if code ~= space then
-			res[#res + 1] = code
-		end
+	w = w:lower()
+
+	for char in w:gmatch "%w" do
+		res[#res + 1] = string.byte(char)
 	end
 
 	table.sort(res)
@@ -63,13 +62,13 @@ local function word_diff(b,a)
 end
 
 local function compare_words(a,b)
-	
+
 	if #a == #b then
 		return a < b
 	else
 		return #a > #b
 	end
-	
+
 end
 
 --[[ Dictionary ]]--
@@ -81,7 +80,7 @@ local function load_dict(words_array)
 	if words_array.is_dict then
 		return words_array
 	end
-	
+
 	table.sort(words_array, compare_words)
 
 	local words = {}
@@ -101,18 +100,18 @@ end
 -- To avoid duplicate answers, always go in decreasing order
 
 local function filter_dict(dict, word, start_at, min_len, yield_often)
-	
+
 	local res = {}
 	local diffs = {}
-	
+
 	for i,v in ipairs(dict) do
-		
+
 		if yield_often and i % 5000 == 0 then
 			coroutine.yield("pause")
 		end
-		
+
 		local skip = #v < min_len or (start_at and compare_words(v.string, start_at.string))
-		
+
 		if not skip then
 		local diff = word_diff(word, v)
 			if diff then
@@ -120,9 +119,9 @@ local function filter_dict(dict, word, start_at, min_len, yield_often)
 				table.insert(diffs, diff)
 			end
 		end
-		
+
 	end
-	
+
 	return res,diffs
 end
 
@@ -139,7 +138,7 @@ local function find_anagrams(dict, word, current, excludes, min_len, yield_often
 	local subs,diffs = filter_dict(dict, word, start_at, min_len, yield_often)
 
 	for i,subword in ipairs(subs) do
-		
+
 		if not excludes[subword.string] then
 
 			local diff = diffs[i]
