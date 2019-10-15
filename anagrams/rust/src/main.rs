56db348f9f370ff2006306281835f9431ef3242c
Noé Falzon <noe@yousician.com>
2019-10-15 17:29:47 +0300

    Formatted

--- anagrams/rust/src/main.rs
+++ anagrams/rust/src/main.rs
@@ -1,51 +1,46 @@
 use std::env;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
 
-	let args : Vec<String> = env::args().collect();
+    if args.len() <= 3 {
+        println!("Usage: anagrams <dict_path> <phrase> [+include] [-exclude] [>min_len]");
+        return;
+    }
 
-	if args.len() <= 3 {
-		println!("Usage: anagrams <dict_path> <phrase> [+include] [-exclude] [>min_len]");
-		return
-	}
+    let dict_path = &args[1];
+    let phrase = &args[2];
 
-	let dict_path = &args[1];
-	let phrase = &args[2];
+    println!("Dict path: {}, phrase: {}", dict_path, phrase);
 
-	println!("Dict path: {}, phrase: {}", dict_path, phrase);
+    let mut includes: Vec<String> = Vec::new();
+    let mut excludes: Vec<String> = Vec::new();
 
-	let mut includes : Vec<String> = Vec::new();
-	let mut excludes : Vec<String> = Vec::new();
+    let mut min_len: Option<u32> = None;
 
-	let mut min_len: Option<u32>  = None;
+    for arg in &args[3..] {
+        if arg.starts_with("+") && arg.len() > 1 {
+            includes.push(arg[1..].to_string());
+        } else if arg.starts_with("-") && arg.len() > 1 {
+            excludes.push(arg[1..].to_string());
+        } else if arg.starts_with(">") && arg.len() > 1 {
+            match arg[1..].parse() {
+                Ok(n) => min_len = Some(n),
+                Err(_) => {
+                    println!("Invalid argument: {}", arg);
+                    return;
+                }
+            }
+        } else {
+            println!("Invalid argument: {}", arg);
+            return;
+        }
+    }
 
-	for arg in &args[3..] {
+    println!("Includes: {}", includes.join(", "));
+    println!("Excludes: {}", excludes.join(", "));
 
-		if arg.starts_with("+") && arg.len() > 1 {
-			includes.push(arg[1..].to_string());
-		}
-		else if arg.starts_with("-") && arg.len() > 1 {
-			excludes.push(arg[1..].to_string());
-		}
-		else if arg.starts_with(">") && arg.len() > 1 {
-			match arg[1..].parse() {
-				Ok(n) => min_len = Some(n),
-				Err(_) => {
-					println!("Invalid argument: {}", arg);
-					return
-				}
-			}
-		}
-		else {
-			println!("Invalid argument: {}", arg);
-			return
-		}
-	}
-
-	println!("Includes: {}", includes.join(", "));
-	println!("Excludes: {}", excludes.join(", "));
-
-	if let Some(n) = min_len {
-		println!("Min len: {}", n);
-	}
+    if let Some(n) = min_len {
+        println!("Min len: {}", n);
+    }
 }
