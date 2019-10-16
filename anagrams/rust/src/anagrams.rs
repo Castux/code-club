7e15f4171bde54569dd043d734dfe6dc19a81c10
Noé Falzon <noe@yousician.com>
2019-10-16 10:46:42 +0300

    Loading dict

--- anagrams/rust/src/anagrams.rs
+++ anagrams/rust/src/anagrams.rs
@@ -1,79 +1,64 @@
 use std::cmp::Ordering;
 
 pub struct Word {
-    chars: Vec<char>,
-    string: String,
+	chars: Vec<char>,
+	string: String,
 }
 
 impl Word {
-    fn len(&self) -> usize {
-        self.chars.len()
-    }
-}
+	fn len(&self) -> usize {
+		self.chars.len()
+	}
 
-impl Ord for Word {
-    fn cmp(&self, other: &Self) -> Ordering {
-        match self.len().cmp(&other.len()) {
-            Ordering::Greater => Ordering::Less,
-            Ordering::Less => Ordering::Greater,
-            Ordering::Equal => self.string.cmp(&other.string),
-        }
-    }
+	fn cmp(&self, other: &Self) -> Ordering {
+		match self.len().cmp(&other.len()) {
+			Ordering::Greater => Ordering::Less,
+			Ordering::Less => Ordering::Greater,
+			Ordering::Equal => self.string.cmp(&other.string),
+		}
+	}
 }
-
-impl PartialOrd for Word {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl PartialEq for Word {
-    fn eq(&self, other: &Self) -> bool {
-        self.string == other.string
-    }
-}
-
-impl Eq for Word {}
 
 pub fn load_word(word: &str) -> Word {
-    let mut chars: Vec<char> = word.chars().collect();
-    chars.sort();
+	let mut chars: Vec<char> = word.chars().collect();
+	chars.sort();
 
-    Word {
-        chars,
-        string: word.to_string(),
-    }
+	Word {
+		chars,
+		string: word.to_string(),
+	}
 }
 
 pub fn word_diff(b: &Word, a: &Word) -> Option<Vec<char>> {
-    let mut res: Vec<char> = Vec::new();
+	let mut res: Vec<char> = Vec::new();
 
-    let mut i: usize = 0;
-    let mut j: usize = 0;
+	let mut i: usize = 0;
+	let mut j: usize = 0;
 
-    while i < a.chars.len() && j < b.chars.len() {
-        if a.chars[i] < b.chars[j] {
-            return None;
-        } else if a.chars[i] == b.chars[j] {
-            i += 1;
-            j += 1;
-        } else {
-            // a[i] > b[j]
-            res.push(b.chars[j]);
-            j += 1;
-        }
-    }
+	while i < a.chars.len() && j < b.chars.len() {
+		if a.chars[i] < b.chars[j] {
+			return None;
+		} else if a.chars[i] == b.chars[j] {
+			i += 1;
+			j += 1;
+		} else {
+			// a[i] > b[j]
+			res.push(b.chars[j]);
+			j += 1;
+		}
+	}
 
-    if i < a.chars.len() {
-        return None;
-    }
+	if i < a.chars.len() {
+		return None;
+	}
 
-    res.extend_from_slice(&b.chars[j..]);
-    Some(res)
+	res.extend_from_slice(&b.chars[j..]);
+	Some(res)
 }
 
-pub fn load_dict(words: &Vec<String>) -> Vec<Word> {
-    let mut res: Vec<Word> = words.iter().map(|w| load_word(&w)).collect();
-    res.sort();
-    res
+pub fn load_dict(words: &Vec<&str>) -> Vec<Word> {
+	let mut res: Vec<Word> = words.iter().map(|w| load_word(w)).collect();
+
+	res.sort_by(Word::cmp);
+	res
 }
--- anagrams/rust/src/main.rs
+++ anagrams/rust/src/main.rs
@@ -1,56 +1,68 @@
 use std::env;
+use std::fs;
 
 mod anagrams;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+	let args: Vec<String> = env::args().collect();
 
-    if args.len() <= 3 {
-        println!("Usage: anagrams <dict_path> <phrase> [+include] [-exclude] [>min_len]");
-        return;
-    }
+	if args.len() < 2 {
+		println!("Usage: anagrams <dict_path> <phrase> [+include] [-exclude] [>min_len]");
+		return;
+	}
 
-    let dict_path = &args[1];
-    let phrase = &args[2];
+	// Arguments parsing
 
-    println!("Dict path: {}, phrase: {}", dict_path, phrase);
+	let dict_path = &args[1];
+	let phrase = &args[2];
 
-    let mut includes: Vec<String> = Vec::new();
-    let mut excludes: Vec<String> = Vec::new();
+	println!("Dict path: {}, phrase: {}", dict_path, phrase);
 
-    let mut min_len: Option<u32> = None;
+	let mut includes: Vec<String> = Vec::new();
+	let mut excludes: Vec<String> = Vec::new();
 
-    for arg in &args[3..] {
-        if arg.starts_with("+") && arg.len() > 1 {
-            includes.push(arg[1..].to_string());
-        } else if arg.starts_with("-") && arg.len() > 1 {
-            excludes.push(arg[1..].to_string());
-        } else if arg.starts_with(">") && arg.len() > 1 {
-            match arg[1..].parse() {
-                Ok(n) => min_len = Some(n),
-                Err(_) => {
-                    println!("Invalid argument: {}", arg);
-                    return;
-                }
-            }
-        } else {
-            println!("Invalid argument: {}", arg);
-            return;
-        }
-    }
+	let mut min_len: Option<u32> = None;
 
-    println!("Includes: {}", includes.join(", "));
-    println!("Excludes: {}", excludes.join(", "));
+	for arg in &args[3..] {
+		if arg.starts_with("+") && arg.len() > 1 {
+			includes.push(arg[1..].to_string());
+		} else if arg.starts_with("-") && arg.len() > 1 {
+			excludes.push(arg[1..].to_string());
+		} else if arg.starts_with(">") && arg.len() > 1 {
+			match arg[1..].parse() {
+				Ok(n) => min_len = Some(n),
+				Err(_) => {
+					println!("Invalid argument: {}", arg);
+					return;
+				}
+			}
+		} else {
+			println!("Invalid argument: {}", arg);
+			return;
+		}
+	}
 
-    if let Some(n) = min_len {
-        println!("Min len: {}", n);
-    }
+	println!("Includes: {}", includes.join(", "));
+	println!("Excludes: {}", excludes.join(", "));
 
-    // Testing
+	if let Some(n) = min_len {
+		println!("Min len: {}", n);
+	}
 
-    let foo = anagrams::load_word("foobar");
-    let bar = anagrams::load_word("rab");
+	// Loading dictionary
 
-    let diff = anagrams::word_diff(&foo, &bar);
-    println!("Diff {:?}", diff);
+	let dict_str = match fs::read_to_string(dict_path) {
+		Err(_) => {
+			println!("Couldn't read file: {}", dict_path);
+			return;
+		}
+		Ok(str) => str,
+	};
+
+	let words: Vec<&str> = dict_str.split_whitespace().collect();
+	let dictionary = anagrams::load_dict(&words);
+
+	// Testing
+
+	println!("Loaded {} words", dictionary.len());
 }
