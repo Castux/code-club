c7aee1c146c852df7f828aed915e9febfb818945
Noé Falzon <noe@yousician.com>
2019-10-16 13:28:27 +0300

    Reorganised folders

---
+++ anagrams/lua/publish.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp anagrams-web.lua anagrams.lua dict.txt fengari-web.js index.html style.css ../../docs/anagrams/
--- anagrams/publish.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cp anagrams-web.lua anagrams.lua dict.txt fengari-web.js index.html style.css ../docs/anagrams/
