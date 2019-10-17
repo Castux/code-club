89209ef60995476ddebb2b40b728b4de940f01a8
Noé Falzon <noe@yousician.com>
2019-10-17 10:34:02 +0300

    Added Finnish dict

--- anagrams/lua/publish.sh
+++ anagrams/lua/publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cp anagrams-web.lua anagrams.lua ../en.txt ../fr.txt fengari-web.js index.html style.css ../../docs/anagrams/
+cp anagrams-web.lua anagrams.lua ../en.txt ../fr.txt ../fi.txt fengari-web.js index.html style.css ../../docs/anagrams/
