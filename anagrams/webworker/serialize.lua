fc871605fbd68509a0aee8d173221f9286600e00
Noé Falzon <falzon.noe@gmail.com>
2020-04-09 00:10:39 +0300

    Better serialize

--- anagrams/webworker/serialize.lua
+++ anagrams/webworker/serialize.lua
@@ -2,14 +2,29 @@ local function serialize(t)
 
 	if type(t) == "table" then
 		local res = {}
+
+        for i,v in ipairs(t) do
+            table.insert(res, serialize(v))
+        end
+
 		for k,v in pairs(t) do
-			table.insert(res, string.format("[%s]=%s", serialize(k), serialize(v)))
+            local is_index =
+                type(k) == "number" and
+                math.tointeger(k) and
+                k >= 1 and
+                k <= #t
+
+            if not is_index then
+			    table.insert(res,
+                    string.format("[%s]=%s", serialize(k), serialize(v))
+                )
+            end
 		end
 
 		return "{" .. table.concat(res, ",") .. "}"
 
 	elseif type(t) == "string" then
-		return "'".. t .. "'"
+		return string.format("%q", t)
 	elseif not t or type(t) == "number" or type(t) == "boolean" then
 		return tostring(t)
 	else
